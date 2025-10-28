e65b5e09efbf829219c3e0a77517700a05b0a532
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-28 21:06:43 +0600

    Create problem-01.cpp

---
+++ problem-01.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/contest/2167/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        string m,s;
+        cin>>n>>m>>s;
+         sort(m.begin(), m.end());
+         sort(s.begin(), s.end());
+        if(m==s){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+        
+    }
+    return 0;
+}
