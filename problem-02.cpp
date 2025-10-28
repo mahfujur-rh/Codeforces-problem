7c8ed4ed8aa9368cdcbc55b5626d26ccfe298c37
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-28 21:07:24 +0600

    Create problem-02.cpp

---
+++ problem-02.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/contest/2167/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int a,b,c,d;cin>>a>>b>>c>>d;
+        if(a==d && b==c){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
