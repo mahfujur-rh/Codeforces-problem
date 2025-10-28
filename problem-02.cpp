2707c3aaa39092f79d01fa4f59aa3b11180404d6
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-28 21:13:19 +0600

    Update problem-02.cpp

--- problem-02.cpp
+++ problem-02.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> tc;
     while (tc--) {
         int a,b,c,d;cin>>a>>b>>c>>d;
-        if(a==d && b==c){
+        if(a == b && b == c && c == d){
             cout<<"YES"<<endl;
         }
         else cout<<"NO"<<endl;
