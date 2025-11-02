e36cd13b97d2c333fd8b00136b80bf2f37e6e90b
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-02 18:49:09 +0600

    Create problem-04.cpp

---
+++ problem-04.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int m,s;cin>>m>>s;
+	int total_domino =m*s/2;
+	cout<<total_domino<<endl;
+	return 0;
+
+}
