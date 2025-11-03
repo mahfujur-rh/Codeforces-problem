01b9e58349423466781f61b611bf6962f1e74e9b
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-03 19:08:32 +0600

    Create problem-06.cpp

---
+++ problem-06.cpp
@@ -0,0 +1,20 @@
+//// https://codeforces.com/problemset/problem/1676/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;cin>>tc;
+    while(tc--){    //string char
+        char a,b,c,d,e,f;cin>>a>>b>>c>>d>>e>>f;
+        int sum1 =a+b+c;
+        int sum2 =d+e+f;
+        if(sum1==sum2){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+    
+    
+    return 0;
+}
